336183325bb3eedf92f772a0be0a47a4e9f95626
Rohit Chauhan <rohit.alok@hotmail.com>
2018-01-19 11:22:47 +0530

    Added - RedBlackBST and HashTable

---
+++ AlgorithmsInCSharp/ArrayBased/HashTable.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsInCSharp.ArrayBased
+{
+    public class HashTable<TKey,TValue> where TKey : IComparable
+    {
+        private int Size = 0;
+        private int tableSize = 16;
+        private TKey[] Keys;
+        private TValue[] Values;
+
+        public HashTable()
+        {
+            Keys = new TKey[tableSize];
+            Values = new TValue[tableSize];
+        }
+
+        public HashTable(int capacity)
+        {
+            Keys = new TKey[capacity];
+            Values = new TValue[capacity];
+            tableSize = capacity;
+        }
+
+        private int GetHash(TKey key)
+        {
+            return (key.GetHashCode() & 0x7fffffff) % tableSize;
+        }
+
+        public TValue Get(TKey key)
+        {
+            int index = GetHash(key);
+            for(int i=index;Keys[i] != null;i = (i+1)%tableSize)
+            {
+                if(key.CompareTo(Keys[i]) == 0)
+                {
+                    return Values[i];
+                }
+            }
+            throw new InvalidOperationException("Key Not Found");
+        }
+
+        public bool Contains(TKey key)
+        {
+            for(int i = GetHash(key);Keys[i] != null;i = (i+1)%tableSize)
+            {
+                if(key.CompareTo(Keys[i]) == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void Put(TKey key,TValue value)
+        {
+            if(Size >= tableSize/2)
+            {
+                Resize(tableSize * 2);
+            }
+            int index;
+
+            for(index= GetHash(key); Keys[index] != null;index = (index+1)%31)
+            {
+                if(key.CompareTo(Keys[index]) == 0)
+                {
+                    Values[index] = value;
+                    return;
+                }
+            }
+            Keys[index] = key;
+            Values[index] = value;
+            Size++;
+
+        }
+
+        public void Delete(TKey key)
+        {
+            if(!Contains(key))
+            {
+                throw new InvalidOperationException("Key Not Found");
+            }
+            int index = GetHash(key);
+            while(key.CompareTo(Keys[index]) != 0)
+            {
+                index = (index + 1) % tableSize;
+            }
+            //TODO handle below for primitive types
+            Keys[index] = default(TKey);
+            Values[index] = default(TValue);
+
+            index = (index + 1) % tableSize;
+            while(Keys[index] != null)
+            {
+                var currentKey = Keys[index];
+                var currentValue = Values[index];
+                Keys[index] = default(TKey);
+                Values[index] = default(TValue);
+                Size--;
+                Put(currentKey, currentValue);
+                index = (index + 1) % tableSize;
+            }
+            Size--;     
+            if(Size > 0 && Size <= tableSize/8)
+            {
+                Resize(tableSize / 2);
+            }
+
+        }
+
+        private void Resize(int capacity)
+        {
+            HashTable<TKey, TValue> temp = new HashTable<TKey, TValue>(capacity);
+            for (int i = 0;i < tableSize;i++)
+            {
+                if(Keys[i] != null)
+                {
+                    temp.Put(Keys[i], Values[i]);
+                }
+            }
+            Keys = temp.Keys;
+            Values = temp.Values;
+            tableSize = temp.tableSize;
+        }
+    }
+}
---
+++ AlgorithmsInCSharp/Linked/RedBlackBST.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsInCSharp.Linked
+{
+    public class RedBlackBST<TKey,TValue> where TKey:IComparable
+    {
+        private static readonly bool BLACK = false;
+        private static readonly bool RED = true;
+
+        private class Node
+        {
+            public TKey Key;
+            public TValue Value;
+            public Node LeftLink;
+            public Node RightLink;
+            public int Size;
+            public bool Color;
+        }
+        private Node root;
+        public int Size()
+        {
+            return Size(root);
+        }
+
+        private int Size(Node node)
+        {
+            if (node == null) return 0;
+            else return node.Size;
+        }
+
+        private Node RotateLeft(Node input)
+        {
+            Node outpt = input.RightLink;
+            input.RightLink = outpt.LeftLink;
+            outpt.LeftLink = input;
+            outpt.Color = input.Color;
+            outpt.Size = input.Size;
+            input.Color = RED;
+            input.Size = 1 + Size(input.LeftLink) + Size(input.RightLink);
+            return outpt;
+        }
+
+        private Node RotateRight(Node input)
+        {
+            Node outpt = input.LeftLink;
+            input.LeftLink = outpt.RightLink;
+            outpt.RightLink = input;
+            outpt.Color = input.Color;
+            outpt.Size = input.Size;
+            input.Color = RED;
+            input.Size = 1 + Size(input.LeftLink) + Size(outpt.RightLink);
+            return outpt;
+        }
+
+        private void FlipColors(Node node)
+        {
+            node.LeftLink.Color = BLACK;
+            node.RightLink.Color = BLACK;
+            node.Color = RED;
+        }
+
+        private bool isRed(Node node)
+        {
+            if(node == null)
+            {
+                return false;
+            }
+            return node.Color == RED;
+        }
+
+        public TValue Get(TKey key)
+        {
+            Node node = Get(root, key);
+            if(node == null)
+            {
+                throw new InvalidOperationException("Key not found");
+            }
+            else
+            {
+                return node.Value;
+            }
+        }
+
+        private Node Get(Node node,TKey key)
+        {
+            if(node == null)
+            {
+                return null;
+            }
+            int cmp = key.CompareTo(node.Key);
+            if(cmp > 0)
+            {
+                return Get(node.RightLink, key);
+            }
+            else if(cmp < 0)
+            {
+                return Get(node.LeftLink, key);
+            }
+            else
+            {
+                return node;
+            }
+        }
+
+        public void Put(TKey key,TValue value)
+        {
+            root = Put(root, key, value);
+            root.Color = BLACK;
+        }
+
+        private Node Put(Node node,TKey key,TValue value)
+        {
+            if(node == null)
+            {
+                return new Node()
+                {
+                    Key = key,
+                    Value = value,
+                    Color = RED,
+                    Size = 1
+                };
+            }
+            int cmp = key.CompareTo(node.Key);
+            if(cmp > 0)
+            {
+                node.RightLink = Put(node.RightLink, key, value);
+            }
+            else if(cmp < 0)
+            {
+                node.LeftLink = Put(node.LeftLink, key, value);
+            }
+            else
+            {
+                node.Value = value;
+            }
+            if(isRed(node.RightLink) && !isRed(node.LeftLink))
+            {
+                RotateLeft(node);
+            }
+            if(isRed(node.LeftLink) && isRed(node.LeftLink.LeftLink))
+            {
+                RotateRight(node);
+            }
+            if(isRed(node.LeftLink) && isRed(node.RightLink))
+            {
+                FlipColors(node);
+            }
+            node.Size = 1 + Size(node.LeftLink) + Size(node.RightLink);
+            return node;
+        }
+    }
+}
